55c3f555122828c8224a8ddee2a6fbb35c9ad5d1
unknown <konr4d97@gmail.com>
2019-01-06 20:44:56 +0100

    6.01.2019

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void InkCanvas1_Gesture(object sender, InkCanvasGestureEventArgs e)
+        {
+        }
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.inkCanvas1.Strokes.Clear();
+            MessageBox.Show("Wyczyszczono projekt");
+        }
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Do zobaczenia :)");
+            this.Close();
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            SaveJPG jpg = new SaveJPG();
+            jpg.ExportToJpeg("F://ProjektwpfV2//Zdjęcia//plik.jpg", inkCanvas1);
+            MessageBox.Show("Obrazek został zapisany pomyślnie.");
+        }
+    }
+}
---
+++ SaveJPG.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows.Media;
+
+namespace Projekt
+{
+    class SaveJPG
+    {
+     public void ExportToJpeg(String path, InkCanvas surface)
+    {
+        double
+                x1 = surface.Margin.Left,
+                x2 = surface.Margin.Top,
+                x3 = surface.Margin.Right,
+                x4 = surface.Margin.Bottom;
+
+        if (path == null) return;
+
+        surface.Margin = new Thickness(0, 0, 0, 0);
+
+        Size size = new Size(surface.Width, surface.Height);
+        surface.Measure(size);
+        surface.Arrange(new Rect(size));
+
+        RenderTargetBitmap renderBitmap =
+         new RenderTargetBitmap(
+           (int)size.Width,
+           (int)size.Height,
+           96,
+           96,
+           PixelFormats.Default);
+        renderBitmap.Render(surface);
+        using (FileStream fs = File.Open(path, FileMode.Create))
+        {
+            JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(renderBitmap));
+            encoder.Save(fs);
+        }
+        surface.Margin = new Thickness(x1, x2, x3, x4);
+    }
+}
+}
